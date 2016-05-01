0714b4e36a341024a85243cf1edbfd435666caaf
Victor Schröder <schrodervictor@gmail.com>
2016-05-01 19:54:12 +0200

    Adds RVM logic to zsh files

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -6,6 +6,7 @@
 
 export NVM_DIR="$HOME/.nvm"
 
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# Activate NVM
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 nvm use --silent stable
---
+++ zsh/rvm.zsh
@@ -0,0 +1,11 @@
+# ~/.zshrc
+#
+# @package myutilitybelt
+# @subpackage zsh
+# @author Victor Schröder <schrodervictor@gmail.com>
+
+# Activate RVM
+source "$HOME/.rvm/scripts/rvm"
+
+# Add RVM to PATH for scripting
+export PATH="$PATH:$HOME/.rvm/bin"
