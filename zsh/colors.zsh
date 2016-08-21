79e0c60747b84f4538800dd189060f54a6db80b7
Victor Schröder <schrodervictor@gmail.com>
2016-08-21 02:07:34 +0200

    Updates zsh/colors configuration

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -2,10 +2,11 @@
 #
 # @package myutilitybelt
 # @subpackage zsh
+# @author Victor Schröder <schrodervictor@gmail.com>
 # @author thiagoalessio <thiagoalessio@me.com>
 
 autoload colors; colors;
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='4;33'
+alias grep='grep --color=auto'
+export GREP_COLORS='mt=03;33:sl=:cx=:fn=33:ln=32:bn=32:se=36'
 export CLICOLOR='auto'
-ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=auto'
