68c1251c18cb0d3e53f9ea61652a7574f4261428
Victor Schröder <schrodervictor@gmail.com>
2018-06-28 13:25:18 +0200

    Adjusts prompt message

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,24 +3,29 @@
 # @package myutilitybelt
 # @subpackage zsh
 # @author Victor Schröder <schrodervictor@gmail.com>
-# @author thiagoalessio <thiagoalessio@me.com>
 
 set prompt_subst
 
 precmd() {
     vcs_info
 
-    PROMPT=$'\n'
+    PROMPT=''
+    RPROMPT=''
+
+    PROMPT+=$'\n'
     PROMPT+="%{$fg[green]%}%D %*%{$reset_color%} "
+    PROMPT+=$'\n'
     PROMPT+="%{$fg[magenta]%}%n%{$reset_color%}"
     PROMPT+='@'
     PROMPT+="%{$fg[yellow]%}%m%{$reset_color%}"
-    PROMPT+=' in '
+    PROMPT+=':'
     PROMPT+="%{$fg[green]%}%~%{$reset_color%}"
-    PROMPT+="${vcs_info_msg_0_}"
+
+    if [ -n "$vcs_info_msg_0_" ]; then
+        PROMPT+=" [${vcs_info_msg_0_#*on }]"
+    fi
+
     PROMPT+=$'\n'
     PROMPT+="${VIMODE}"
-    PROMPT+="%(?,%{$fg[green]%},%{$fg[red]%})%#%{$reset_color%} "
+    PROMPT+="%(?,%{$fg[green]%},%{$fg[red]%}[%?] )\$%{$reset_color%} "
 }
-
-RPROMPT=''
