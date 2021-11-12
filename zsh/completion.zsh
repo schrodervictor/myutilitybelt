38c9d8d381c8f8662348e8259dfbd644a15d3efc
SerikDM <serik.cre@gmail.com>
2021-11-12 11:11:26 +0100

    Add kubectl autocompletion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -12,3 +12,6 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+# kubectl
+source <(kubectl completion zsh)
