9768e7cb7c62555513255b726bcb9e072a78cb9c
SerikDM <serik.cre@gmail.com>
2021-11-12 11:09:53 +0100

    Support date param in `gl` alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@
 # @author Victor Schröder <schrodervictor@gmail.com>
 
 alias vi='vim'
-alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %Cblue%an%Creset - %s %Cgreen(%cr)%Creset'\'' --abbrev-commit --date=relative'
+alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %Cblue%an%Creset - %s %Cgreen(%cd)%Creset'\'' --abbrev-commit --date=relative'
 alias etc="etc_git_control"
 alias rebar3="$HOME/Projects/community/rebar3/_build/default/bin/rebar3"
 alias tree='tree -I node_modules'
